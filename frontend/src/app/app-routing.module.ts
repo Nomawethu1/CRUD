1238efa3dcbbacac33f943637685dfeddd7ff8e4
Nomawethu <nomawethu.Khoza@dadevs.co.za>
2022-07-11 16:32:12 +0200

    add item

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './Components/login/login.component';
+import { AddItemComponent } from './Components/add-item/add-item.component';
 
-const routes: Routes = [{path:'login',component:LoginComponent}];
+const routes: Routes = [{path:'login',component:LoginComponent},
+{path:'additem',component: AddItemComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

f1bc82c60a626b2185c143ff78d588d042772839
Nomawethu <nomawethu.Khoza@dadevs.co.za>
2022-07-12 10:53:06 +0200

    local storage

--- frontend/src/app/Components/add-item/add-item.component.ts
+++ frontend/src/app/Components/add-item/add-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-item',
@@ -7,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddItemComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup = new FormGroup({
+    tittle: new FormControl(''),
+    description: new FormControl(''),
+    due_Date: new FormControl(''),
+
+  });
+  submitted = false;
+//for getting items from local storage
+  dummy:any;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group(
+      {
+        title: [''],
+        description: [''],
+        due_Date: [''],
+      }
+    )
+//for getting items from local storage
+    this.dummy = localStorage.getItem("form")
+  }
+
+
+
+  onSubmit(): void {
+
+    console.log(JSON.stringify(this.form.value, null, 2));
+
+    localStorage.setItem("form",JSON.stringify(this.form.value, null, 2))
   }
 
 }

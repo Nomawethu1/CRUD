053b3ebebc39ba4d76ba0126f1052beee1be0b69
DA_MAC_58 <da_mac_58@DAs-MacBook-Air-2.local>
2022-07-13 08:43:40 +0200

    login

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './Components/register/register.component';
-import { LoginComponent } from './Components/login/login.component';
 import { AddItemComponent } from './Components/add-item/add-item.component';
 import { DeleteComponent } from './Components/delete/delete.component';
 import { ViewItemComponent } from './Components/view-item/view-item.component';
@@ -11,6 +10,7 @@ import { LogoutComponent } from './Components/logout/logout.component';
 import { EditItemComponent } from './Components/edit-item/edit-item.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FormvalidationComponent } from './Components/formvalidation/formvalidation.component';
+import { LoginComponent } from './Components/login/login.component';
 import { AppRoutingModule } from './app-routing.module';
 
 // Added imports
